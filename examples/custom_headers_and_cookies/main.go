1e2df691475be0f2c459f8aff74b87c241d7575e
Jean Carlos Molossi <jean.carlosmolossi@gmail.com>
2025-02-23 03:57:07 -0300

    chore: new example

---
+++ examples/custom_headers_and_cookies/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"log/slog"
+	"net/http"
+
+	"github.com/jeanmolossi/MaiGo/examples/testserver"
+	"github.com/jeanmolossi/MaiGo/pkg/maigo"
+	"github.com/jeanmolossi/MaiGo/pkg/maigo/mime"
+)
+
+func main() {
+	testsrv := testserver.NewManager().
+		NewServerBuilder().
+		EnableHeaderDebug().
+		Build()
+
+	defer testsrv.Close()
+
+	client := maigo.NewClient(testsrv.URL).
+		Header().AddUserAgent("MaiGo/1.0").
+		Header().Add("X-My-Header", "header-value").
+		Cookie().Add(&http.Cookie{
+		Name:  "__Secure-session-id",
+		Value: "xyz",
+	}).Build()
+
+	resp, err := client.GET("/resources").
+		Header().AddAccept(mime.JSON).
+		Send()
+	if err != nil {
+		slog.Error("Error sending the request.", "error", err)
+	}
+
+	var data []testserver.Resource
+
+	if err := resp.Body().AsJSON(&data); err != nil {
+		slog.Error("Error parsing response", "error", err)
+		return
+	}
+
+	slog.Info("Data received!", "data", data)
+}
