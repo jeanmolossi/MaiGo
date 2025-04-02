1427d5ad7c368e3952d85179b46b9ffc215e2d69
Jean Carlos Molossi <jean.carlosmolossi@gmail.com>
2025-04-02 20:30:19 -0300

    chore: add example with load balancing

---
+++ examples/load_balancing/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"log/slog"
+
+	"github.com/jeanmolossi/MaiGo/examples/testserver"
+	"github.com/jeanmolossi/MaiGo/pkg/maigo"
+	"github.com/jeanmolossi/MaiGo/pkg/maigo/contracts"
+)
+
+func main() {
+	serverManager := testserver.NewManager()
+
+	ts1 := serverManager.NewServer()
+	defer ts1.Close()
+
+	ts2 := serverManager.NewServerBuilder().EnableBusy().Build()
+	defer ts2.Close()
+
+	ts3 := serverManager.NewServer()
+	defer ts3.Close()
+
+	client := maigo.NewClientLoadBalancer([]string{
+		ts1.URL,
+		ts2.URL,
+		ts3.URL,
+	}).Build()
+
+	for range 100 {
+		healthcheck(client)
+	}
+}
+
+func healthcheck(client contracts.ClientHTTPMethods) {
+	resp, err := client.GET("/").Send()
+	if err != nil {
+		slog.Error("error sending the request", "error", err)
+		return
+	}
+
+	if resp.Status().Is5xxServerError() {
+		slog.Error("healthcheck failed.", "status", resp.Status().Text())
+		return
+	}
+
+	var healthResponse map[string]any
+
+	if err := resp.Body().AsJSON(&healthResponse); err != nil {
+		slog.Error("error parsing response.", "error", err)
+		return
+	}
+
+	slog.Info("healthcheck received.", "data", healthResponse)
+}
