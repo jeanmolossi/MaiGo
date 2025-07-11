e6bc397c7eac6dbb33e772fd2f5c4bbb07e9351f
Jean Carlos Molossi <jean.carlosmolossi@gmail.com>
2025-07-10 23:33:45 -0300

    feat: implemented context builder

--- pkg/maigo/contracts/request.go
+++ pkg/maigo/contracts/request.go
@@ -4,6 +4,7 @@ type RequestBuilder interface {
 	Header() BuilderHeader[RequestBuilder]
 	Body() BuilderRequestBody[RequestBuilder]
 	Retry() BuilderRequestRetry[RequestBuilder]
+	Context() BuilderRequestContext[RequestBuilder]
 
 	Send() (Response, error)
 }
---
+++ pkg/maigo/request_context_builder.go
@@ -0,0 +1,27 @@
+package maigo
+
+import (
+	"context"
+
+	"github.com/jeanmolossi/MaiGo/pkg/maigo/contracts"
+)
+
+var _ contracts.BuilderRequestContext[contracts.RequestBuilder] = (*RequestContextBuilder)(nil)
+
+type RequestContextBuilder struct {
+	parent *RequestBuilder
+	config *RequestConfigBase
+}
+
+func (r *RequestBuilder) Context() contracts.BuilderRequestContext[contracts.RequestBuilder] {
+	return &RequestContextBuilder{
+		parent: r,
+		config: r.request.config,
+	}
+}
+
+// Set implements contracts.BuilderRequestContext.
+func (r *RequestContextBuilder) Set(ctx context.Context) contracts.RequestBuilder {
+	r.config.Context().Set(ctx)
+	return r.parent
+}
