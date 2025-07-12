a436a97c08a9ecf8ce2276e69cf808a6f2bbb295
Jean Carlos Molossi <jean.carlosmolossi@gmail.com>
2025-07-12 14:32:12 -0300

    docs(example): add godoc for testserver

--- examples/testserver/doc.go
+++ examples/testserver/doc.go
@@ -1,2 +1,3 @@
-// Package testserver
+// Package testserver provides a configurable HTTP test server with delay
+// simulation capabilities for testing concurrent request scenarios.
 package testserver
