30c2f838c7a0c06323c06d63fb275e3d44bf332f
Jean Carlos Molossi <jean.carlosmolossi@gmail.com>
2025-07-11 21:44:31 -0300

    fix request validation error message (#3)

--- pkg/maigo/errors.go
+++ pkg/maigo/errors.go
@@ -6,7 +6,7 @@ var (
 	ErrEmptyBaseURL      = errors.New("empty base URL is not allowed")
 	ErrParseURL          = errors.New("failed to parse URL")
 	ErrClientValidation  = errors.New("invalid client attributes")
-	ErrRequestValidation = errors.New("invalid client attributes")
+	ErrRequestValidation = errors.New("invalid request attributes")
 	ErrCreateRequest     = errors.New("failed to create request")
 	ErrParseProxyURL     = errors.New("failed to parse proxy url")
 	ErrToSetBody         = errors.New("failed to set body")
