8ae5dbd7295add371b75507ccb20802a59aefe00
Jean Carlos Molossi <jean.carlosmolossi@gmail.com>
2025-08-24 02:01:07 -0300

    test: strengthen context wrapper tests

--- pkg/maigo/context_test.go
+++ pkg/maigo/context_test.go
@@ -11,10 +11,40 @@ func TestContextSetIgnoresNil(t *testing.T) {
 	c := newDefaultContext()
 	ctxBefore := c.Unwrap()
 
-	c.Set(nil)
+	c.Set(nil) //nolint:staticcheck // testing nil handling
 
 	if c.Unwrap() != ctxBefore {
 		t.Errorf("Set(nil) changed context: got %v, want %v", c.Unwrap(), ctxBefore)
+	}
+}
+
+func TestContextSetNilReceiver(t *testing.T) {
+	t.Parallel()
+
+	var c *Context
+
+	func() {
+		defer func() {
+			if r := recover(); r != nil {
+				t.Fatalf("Set() panicked: %v", r)
+			}
+		}()
+		c.Set(context.Background())
+	}()
+}
+
+type ctxKey struct{}
+
+func TestContextSetReplacesContext(t *testing.T) {
+	t.Parallel()
+
+	c := newDefaultContext()
+	ctx := context.WithValue(context.Background(), ctxKey{}, "v")
+
+	c.Set(ctx)
+
+	if got := c.Unwrap(); got != ctx {
+		t.Fatalf("Unwrap() = %v, want %v", got, ctx)
 	}
 }
 
@@ -37,6 +67,9 @@ func BenchmarkContextSet(b *testing.B) {
 	c := newDefaultContext()
 	ctx := context.Background()
 
+	b.ReportAllocs()
+	b.ResetTimer()
+
 	for i := 0; i < b.N; i++ {
 		c.Set(ctx)
 	}
@@ -44,6 +77,9 @@ func BenchmarkContextSet(b *testing.B) {
 
 func BenchmarkContextUnwrap(b *testing.B) {
 	c := newDefaultContext()
+
+	b.ReportAllocs()
+	b.ResetTimer()
 
 	for i := 0; i < b.N; i++ {
 		_ = c.Unwrap()
